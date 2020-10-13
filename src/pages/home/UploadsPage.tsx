f43476a695bc071a055bd0bd78f7d4e73abada7e
Rodrigo Fonseca <ro.fonseca.melo@gmail.com>
2020-10-13 14:54:52 -0300

    refactor(altera nome de arquivo): altera o arquivo: HomePage -> UploadsPage

---
+++ src/pages/home/UploadsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormUploadsComponent from 'components/FormUploadsComponent'
+
+function UploadsPage() {
+  return (
+    <div>
+      <FormUploadsComponent />
+    </div>
+  )
+}
+
+export default UploadsPage
