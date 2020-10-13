51112cc9d611a9df4da12c12bb5ab75366ab61bf
Rodrigo Fonseca <ro.fonseca.melo@gmail.com>
2020-10-13 16:44:27 -0300

    style(adiciona o estilo de bootstrap): adiciona estilo e componentes de bootstrap

--- src/components/FormUploadsComponent.tsx
+++ src/components/FormUploadsComponent.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import UploadsResourceService from 'services/UploadsResourceService'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 
-const Wrapper = styled.div``
+const Wrapper = styled(Container)`
+  padding: 10px;
+  border-bottom: 1px solid #f0f0f0;
+
+  .send-btn-wrapper {
+    margin-top: 10px;
+  }
+`
 
 const FormUploadsComponent = () => {
   const [formData, setFormData] = useState<FormData | undefined>()
@@ -29,14 +37,33 @@ const FormUploadsComponent = () => {
   }
 
   return (
-    <Wrapper>
-      <form noValidate>
-        <input type="file" name="file" onChange={onFileUpload} />
+    <Wrapper fluid>
+      <Row>
+        <Col>
+          <Form noValidate>
+            <Form.Row>
+              <Col>
+                <Form.Control type="email" placeholder="Nome do arquivo..." />
+              </Col>
+              <Col>
+                <Form.File
+                  id="exampleFormControlFile1"
+                  name="file"
+                  onChange={onFileUpload}
+                />
+              </Col>
+            </Form.Row>
 
-        <button type="button" onClick={submit}>
-          Submit
-        </button>
-      </form>
+            <Form.Row className="send-btn-wrapper">
+              <Col>
+                <Button type="button" variant="primary" onClick={submit}>
+                  Enviar
+                </Button>
+              </Col>
+            </Form.Row>
+          </Form>
+        </Col>
+      </Row>
     </Wrapper>
   )
 }
