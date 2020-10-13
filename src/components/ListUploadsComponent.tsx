01834f1fbd25d9f35a318886bdd013e4776c2615
Rodrigo Fonseca <ro.fonseca.melo@gmail.com>
2020-10-13 16:43:49 -0300

    feat(adiciona o componente de upload): adiciona o componente de upload na listagem do mesmo

--- src/components/ListUploadsComponent.tsx
+++ src/components/ListUploadsComponent.tsx
@@ -3,8 +3,17 @@ import styled from 'styled-components'
 import UploadsResourceService from 'services/UploadsResourceService'
 import UploadComponent from 'components/UploadComponent'
 import { UploadI } from 'interfaces/upload'
+import { Container, Row, Col } from 'react-bootstrap'
 
-const Wrapper = styled.div``
+const Wrapper = styled(Container)`
+  padding-top: 10px;
+`
+
+const UploadRow = styled(Row)``
+
+const UploadCol = styled(Col)`
+  padding-bottom: 10px;
+`
 
 const ListUploadsComponent = () => {
   const [uploads, setUploads] = useState<Array<UploadI>>([])
@@ -20,11 +29,19 @@ const ListUploadsComponent = () => {
 
   function renderUploads(): Array<JSX.Element> {
     return uploads.map((upload: UploadI) => (
-      <UploadComponent key={upload.id} upload={upload} />
+      <UploadCol key={upload.id}>
+        <UploadComponent upload={upload} />
+      </UploadCol>
     ))
   }
 
-  return <Wrapper>{renderUploads()}</Wrapper>
+  return (
+    <Wrapper fluid>
+      <UploadRow md={2} xs={1} sm={1} lg={4}>
+        {renderUploads()}
+      </UploadRow>
+    </Wrapper>
+  )
 }
 
 export default ListUploadsComponent
