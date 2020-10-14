243d9b20f6fe27cb43b2a30e760bb3241debf9bf
Rodrigo Fonseca <ro.fonseca.melo@gmail.com>
2020-10-13 21:25:22 -0300

    refactor(refatora o código): remove o componente de FormUploads

--- src/components/ListUploadsComponent.tsx
+++ src/components/ListUploadsComponent.tsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import UploadsResourceService from 'services/UploadsResourceService'
 import UploadComponent from 'components/UploadComponent'
 import { UploadI } from 'interfaces/upload'
 import { Container, Row, Col } from 'react-bootstrap'
-import LoadingComponent from 'components/LoadingComponent'
 
 const Wrapper = styled(Container)`
   padding-top: 10px;
@@ -16,20 +14,11 @@ const UploadCol = styled(Col)`
   padding-bottom: 10px;
 `
 
-const ListUploadsComponent = () => {
-  const [uploads, setUploads] = useState<Array<UploadI>>([])
-  const [loading, setLoading] = useState(true)
+interface PropsI {
+  uploads: Array<UploadI>
+}
 
-  useEffect(get, [])
-
-  function get() {
-    const uploadsResourceService = new UploadsResourceService()
-    uploadsResourceService.get().then((response: { data: Array<UploadI> }) => {
-      setUploads(response.data)
-      setLoading(false)
-    })
-  }
-
+const ListUploadsComponent = ({ uploads }: PropsI) => {
   function renderUploads(): Array<JSX.Element> {
     return uploads.map((upload: UploadI) => (
       <UploadCol key={upload.id}>
@@ -39,15 +28,11 @@ const ListUploadsComponent = () => {
   }
 
   return (
-    <>
-      <LoadingComponent loading={loading} />
-
-      <Wrapper fluid>
-        <UploadRow md={2} xs={1} sm={1} lg={4}>
-          {renderUploads()}
-        </UploadRow>
-      </Wrapper>
-    </>
+    <Wrapper fluid>
+      <UploadRow md={2} xs={1} sm={1} lg={4}>
+        {renderUploads()}
+      </UploadRow>
+    </Wrapper>
   )
 }
 
--- src/pages/UploadsPage.tsx
+++ src/pages/UploadsPage.tsx
@@ -1,13 +1,124 @@
-import React from 'react'
-import FormUploadsComponent from 'components/FormUploadsComponent'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import ListUploadsComponent from 'components/ListUploadsComponent'
+import UploadsResourceService from 'services/UploadsResourceService'
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'
+import LoadingComponent from 'components/LoadingComponent'
+import { UploadI } from 'interfaces/upload'
+
+const FormWrapper = styled(Container)`
+  padding: 10px;
+  border-bottom: 1px solid #f0f0f0;
+
+  .send-btn-wrapper {
+    margin-top: 10px;
+  }
+`
 
 function UploadsPage() {
+  const [uploads, setUploads] = useState<Array<UploadI>>([])
+  const [formData, setFormData] = useState<FormData | undefined>()
+  const [fileName, setFileName] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  useEffect(getUploads, [])
+
+  function getUploads() {
+    const uploadsResourceService = new UploadsResourceService()
+
+    uploadsResourceService
+      .get()
+      .then((response: { data: Array<UploadI> }) => {
+        setUploads(response.data)
+      })
+      .catch(() => {
+        alert('Ops, something goes wrong :(')
+      })
+      .finally(() => setLoading(false))
+  }
+
+  function onFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    if (!event.target) return
+    if (!event.target.files) return
+    if (!event.target.files[0]) return
+
+    handleFormData(event.target.files[0])
+  }
+
+  function onFileName(event: React.ChangeEvent<HTMLInputElement>) {
+    if (!event.target) return
+    if (!event.target.value) return
+
+    setFileName(event.target.value)
+  }
+
+  function handleFormData(file: File) {
+    const formData = new FormData()
+    formData.append('file', file)
+
+    setFormData(formData)
+  }
+
+  function submit(e: React.FormEvent<HTMLElement>) {
+    e.preventDefault()
+
+    if (!fileName) return alert('What is the filename?')
+
+    setLoading(true)
+
+    const uploadsResourceService = new UploadsResourceService()
+    uploadsResourceService
+      .create(formData)
+      .then(res => {
+        setUploads([...uploads, ...res.data])
+        setFileName('')
+        setFormData(undefined)
+      })
+      .catch(() => {
+        alert('Ops, something goes wrong :(')
+      })
+      .finally(() => setLoading(false))
+  }
+
   return (
-    <div>
-      <FormUploadsComponent />
-      <ListUploadsComponent />
-    </div>
+    <>
+      <LoadingComponent loading={loading} />
+
+      <FormWrapper fluid>
+        <Row>
+          <Col>
+            <Form onSubmit={submit} noValidate>
+              <Form.Row>
+                <Col>
+                  <Form.Control
+                    type="text"
+                    placeholder="Nome do arquivo..."
+                    onChange={onFileName}
+                  />
+                </Col>
+                <Col>
+                  <Form.File
+                    id="exampleFormControlFile1"
+                    name="file"
+                    onChange={onFileUpload}
+                  />
+                </Col>
+              </Form.Row>
+
+              <Form.Row className="send-btn-wrapper">
+                <Col>
+                  <Button type="submit" variant="primary">
+                    Enviar
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+          </Col>
+        </Row>
+      </FormWrapper>
+
+      <ListUploadsComponent uploads={uploads} />
+    </>
   )
 }
 
